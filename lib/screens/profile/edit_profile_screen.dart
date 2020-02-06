31edeafbd8290b3d1a1bc3d64f0cf1d948430f7a
lordvidex <lordvidex.prime@gmail.com>
2020-02-06 23:57:08 +0300

    Added UI only of TextEditors

--- lib/screens/profile/edit_profile_screen.dart
+++ lib/screens/profile/edit_profile_screen.dart
@@ -13,6 +13,7 @@ class EditProfileScreen extends StatefulWidget {
 
 class _EditProfileScreenState extends State<EditProfileScreen> {
   Gender _gender;
+  Gender _remoteGender;
   bool _isLoading = false;
   final GlobalKey<FormState> _formKey = GlobalKey();
 
@@ -25,6 +26,7 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
   void initState() {
     try {
       Provider.of<Auth>(context, listen: false).getUserGender().then((g) {
+        _remoteGender = g;
         _gender = g;
       });
     } catch (e) {
@@ -33,17 +35,46 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
     super.initState();
   }
 
-  void updateProfile(BuildContext context) {
+  void updateProfile(BuildContext context) async {
+    bool errorOccured = false;
     if (!_formKey.currentState.validate()) {
       return;
     }
     setState(() {
       _isLoading = true;
     });
-
+    //TODO: Error handlings
+    if (_gender != _remoteGender) {
+      await Provider.of<Auth>(context).setUserGender(_gender);
+    }
     setState(() {
       _isLoading = false;
     });
+    await showDialog(
+      context: context,
+      builder: (ctx) => AlertDialog(
+        title: Row(
+          children: [
+            Icon(Icons.warning, color: Colors.red),
+            SizedBox(width: 20),
+            Text('Warning'),
+          ],
+        ),
+        content: Text(
+            'Are you sure you want to leave this page without saving changes?'),
+        actions: <Widget>[
+          FlatButton(
+              child: Text('Yes'),
+              onPressed: () => Navigator.of(context).pop(true)),
+          RaisedButton(
+            color: Colors.black,
+            onPressed: () => Navigator.of(context).pop(false),
+            child: Text('No'),
+          ),
+        ],
+      ),
+    );
+    Navigator.of(context).pop();
   }
 
   @override
@@ -84,62 +115,109 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
             child: Form(
               key: _formKey,
               child: Consumer<Auth>(builder: (_, auth, __) {
-                return Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Hero(
-                      tag: 'profilepic',
-                      child: CircleAvatar(
-                        minRadius: 30,
-                        maxRadius: mediaquery.size.width * 0.3,
-                        backgroundImage: NetworkImage(
-                            auth.user.photoUrl == '' ||
-                                    auth.user.photoUrl == null
-                                ? kProfileImage
-                                : auth.user.photoUrl),
+                return Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Hero(
+                        tag: 'profilepic',
+                        child: CircleAvatar(
+                          minRadius: 30,
+                          maxRadius: mediaquery.size.width * 0.3,
+                          backgroundImage: NetworkImage(
+                              auth.user.photoUrl == '' ||
+                                      auth.user.photoUrl == null
+                                  ? kProfileImage
+                                  : auth.user.photoUrl),
+                        ),
                       ),
-                    ),
-                    FlatButton(
-                      child: Text('UPLOAD PICTURE'),
-                      onPressed: () {},
-                    ),
-                    SizedBox(height: 30),
-                    // TextFormField(),
-                    // TextFormField(),
-                    // TextFormField(),
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: RadioListTile(
-                            title: Text('Male'),
-                            value: Gender.Male,
-                            groupValue: _gender,
-                            onChanged: (gen) {
-                              setState(() {
-                                _gender = gen;
-                              });
-                            },
-                          ),
+                      FlatButton(
+                        child: Text('UPLOAD PICTURE'),
+                        onPressed: () {},
+                      ),
+                      SizedBox(height: 30),
+                      InputDecorator(
+                        child: Text(auth.user.email),
+                        decoration: InputDecoration(
+                          labelText: 'Email',
+                          enabled: false,
+                          border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 2),
+                              borderRadius: BorderRadius.circular(4)),
                         ),
-                        Expanded(
-                          child: RadioListTile(
-                            title: Text('Female'),
-                            value: Gender.Female,
-                            groupValue: _gender,
-                            onChanged: (gen) => setState(
-                              () {
-                                _gender = gen;
-                              },
+                      ),
+                      SizedBox(height: 30),
+                      TextFormField(
+                        validator: (val) {
+                          return null;
+                        },
+                        decoration: InputDecoration(
+                          labelText: 'Old Password',
+                          labelStyle: TextStyle(color: Colors.black),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.blue, width: 2),
+                              borderRadius: BorderRadius.circular(4)),
+                          border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 2),
+                              borderRadius: BorderRadius.circular(4)),
+                        ),
+                      ),
+                      SizedBox(height: 30),
+                      TextFormField(
+                        decoration: InputDecoration(
+                          labelText: 'New Password',
+                          labelStyle: TextStyle(color: Colors.black),
+                          focusedBorder: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.blue, width: 2),
+                              borderRadius: BorderRadius.circular(4)),
+                          border: OutlineInputBorder(
+                              borderSide:
+                                  BorderSide(color: Colors.grey, width: 2),
+                              borderRadius: BorderRadius.circular(4)),
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 16),
+                        child: Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: RadioListTile(
+                                title: Text('Male'),
+                                value: Gender.Male,
+                                groupValue: _gender,
+                                onChanged: (gen) {
+                                  setState(() {
+                                    _gender = gen;
+                                  });
+                                },
+                              ),
                             ),
-                          ),
+                            Expanded(
+                              child: RadioListTile(
+                                title: Text('Female'),
+                                value: Gender.Female,
+                                groupValue: _gender,
+                                onChanged: (gen) => setState(
+                                  () {
+                                    _gender = gen;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
-                    RaisedButton(
-                      child: Text('Save'.toUpperCase()),
-                      onPressed: () => updateProfile(context),
-                    ),
-                  ],
+                      ),
+                      RaisedButton(
+                        child: Text('Save'.toUpperCase()),
+                        onPressed: () => updateProfile(context),
+                      ),
+                    ],
+                  ),
                 );
               }),
             ),
