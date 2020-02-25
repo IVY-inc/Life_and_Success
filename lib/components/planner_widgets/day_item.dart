ec91f5a03d1fd0068379be3266b07ad23e77b68a
lordvidex <lordvidex.prime@gmail.com>
2020-02-26 01:00:17 +0300

    Even landscape view is sexy, bugs with time fixed, Timer.periodic also added to get time every two minutes

--- lib/components/planner_widgets/day_item.dart
+++ lib/components/planner_widgets/day_item.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../../models/constants.dart';
 
 class DayItem extends StatelessWidget {
+  final int ix;
   final DateTime datetime;
   final List<Todo> todos;
-  const DayItem(this.datetime, {this.todos = const []});
+  const DayItem(this.ix,this.datetime, {this.todos = const []});
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16.0,vertical:2),
       child: Column(
         children: <Widget>[
-          Container(
-            child: Row(children: [
-              Text(
-                datetime.difference(DateTime.now()) < Duration(days: 1)
-                    ? 'Today'
-                    : datetime.difference(DateTime.now()) < Duration(days: 2)
-                        ? 'Tomorrow'
-                        : week[datetime.weekday],
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
-              ),
-              Text(datetime.toIso8601String())
-            ]),
-          ),
+          Row(children: [
+            Text(
+              ix==0
+                  ? 'Today'
+                  : ix==1
+                      ? 'Tomorrow'
+                      : week[datetime.weekday],
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),
+            ),
+            SizedBox(width:10),
+            Text(DateFormat('EEE MMM d').format(datetime),),
+          ]),
           for (int i = 0; i < todos.length; i++) ...[
             TodoBuilder(todos[i]),
             Divider(),
--- lib/screens/planner_screen.dart
+++ lib/screens/planner_screen.dart
@@ -1,39 +1,72 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 import '../components/planner_widgets/day_item.dart';
 
-class PlannerScreen extends StatelessWidget {
-  bool added = false;
-  int pluspos;
-  Widget addButton=Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            Container(alignment: Alignment.centerLeft, height: 30,width:double.infinity,color: Colors.grey[300], child: Text('+ Add task')),
-            Container(height: 20),
-          ],
-        ),
-        Positioned(bottom:0,child:
-        FloatingActionButton(mini: true,
-          child: Icon(Icons.add),
-          onPressed: () {},
-        )
-        ),
-      ],
-    );
+class PlannerScreen extends StatefulWidget {
+  @override
+  _PlannerScreenState createState() => _PlannerScreenState();
+}
 
-  List<Widget> _list = List.generate(30,
-      (int ix) => DayItem(DateTime.now().add(Duration(days: ix, minutes: 1))));
+class _PlannerScreenState extends State<PlannerScreen> {
+  List<Widget> _list;
+  Timer timer;
+  @override
+  void initState() {
+    updateList();
+    timer = Timer.periodic(Duration(minutes: 2),(t)=>setState(()=>updateList()));
+    super.initState();
+  }
+  @override
+  void dispose(){
+    timer.cancel();
+    super.dispose();
+  }
+  int pluspos;
+  Widget addButton = Stack(
+    alignment: Alignment.center,
+    children: <Widget>[
+      Column(
+        children: <Widget>[
+          Container(
+              alignment: Alignment.centerLeft,
+              height: 30,
+              width: double.infinity,
+              color: Colors.grey[300],
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal:16.0),
+                child: Text('+ Add task'),
+              )),
+          Container(height: 20),
+        ],
+      ),
+      Positioned(
+          bottom: 0,
+          child: FloatingActionButton(
+            mini: true,
+            child: Icon(Icons.add),
+            onPressed: () {},
+          )),
+    ],
+  );
+
   void updateList() {
+    _list = List.generate(
+      30,
+      (int ix) => DayItem(
+        ix,
+        DateTime.now().add(
+          Duration(
+            hours: ix * 24,
+          ),
+        ),
+      ),
+    );
     for (int i = 0; i < _list.length; i++) {
-      if ((_list[i] as DayItem).todos.isEmpty &&!added) {
+      if ((_list[i] as DayItem).todos.isEmpty) {
         pluspos = i + 1;
-        _list.insert(
-          pluspos,
-          addButton
-        );
-        added = true;
+        _list.insert(pluspos, addButton);
         break;
       }
     }
@@ -41,17 +74,18 @@ class PlannerScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if(!added)updateList();
     return Scaffold(
       appBar: AppBar(
         title: Text('Planner'),
       ),
       body: Padding(
-        padding: const EdgeInsets.symmetric(vertical:8.0),
+        padding: const EdgeInsets.symmetric(vertical: 12.0),
         child: ListView.separated(
           itemBuilder: (ctx, i) => _list[i],
           itemCount: _list.length,
-          separatorBuilder: (_,i) => i==pluspos-1||i==pluspos?Container(height:4):Divider(),
+          separatorBuilder: (_, i) => i == pluspos - 1 || i == pluspos
+              ? Container(height: 4)
+              : Divider(),
         ),
       ),
     );
